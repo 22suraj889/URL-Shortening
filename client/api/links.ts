af957cc834c9481ed1aaba038aa13354e44f4c0e
Suraj Poddar <surajku2022@gmail.com>
2025-11-23 10:13:09 +0530

    updated with backend url

--- client/api/links.ts
+++ client/api/links.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "url-shortening-sepia-iota.vercel.app",
+  baseURL: "http://url-shortening-sepia-iota.vercel.app", // example: http://localhost:5000
   headers: {
     "Content-Type": "application/json",
   },
